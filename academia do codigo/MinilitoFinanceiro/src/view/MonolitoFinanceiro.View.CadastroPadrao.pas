edaaa8496cb464f45eed0ad0f8d10466072791c2
NathanBnu <nathan.andrade1504@gmail.com>
2022-12-28 08:51:05 -0300

    Estudos em casa.

---
+++ academia do codigo/MinilitoFinanceiro/src/view/MonolitoFinanceiro.View.CadastroPadrao.pas
@@ -0,0 +1,68 @@
+unit MonolitoFinanceiro.View.CadastroPadrao;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.WinXPanels, Data.DB,
+  Vcl.StdCtrls, Vcl.Grids, Vcl.DBGrids, System.ImageList, Vcl.ImgList;
+
+type
+  TfrmCadastroPadrao = class(TForm)
+    pnlPrincipal: TCardPanel;
+    CardCadastro: TCard;
+    CardPesquisa: TCard;
+    pnlPesquisa: TPanel;
+    Panel1: TPanel;
+    pnlGrid: TPanel;
+    DBGrid1: TDBGrid;
+    edtPesquisar: TEdit;
+    Label1: TLabel;
+    btnPesquisar: TButton;
+    ImageList1: TImageList;
+    btnFechar: TButton;
+    btnImprimir: TButton;
+    btnIncluir: TButton;
+    btnAlterar: TButton;
+    btnExcluir: TButton;
+    Panel2: TPanel;
+    btnSalvar: TButton;
+    btnCancelar: TButton;
+    procedure btnIncluirClick(Sender: TObject);
+    procedure btnAlterarClick(Sender: TObject);
+    procedure btnFecharClick(Sender: TObject);
+    procedure btnCancelarClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmCadastroPadrao: TfrmCadastroPadrao;
+
+implementation
+
+{$R *.dfm}
+
+procedure TfrmCadastroPadrao.btnAlterarClick(Sender: TObject);
+begin
+  pnlPrincipal.ActiveCard := cardCadastro;
+end;
+
+procedure TfrmCadastroPadrao.btnCancelarClick(Sender: TObject);
+begin
+  pnlPrincipal.ActiveCard := cardPesquisa;
+end;
+
+procedure TfrmCadastroPadrao.btnFecharClick(Sender: TObject);
+begin
+  close;
+end;
+
+procedure TfrmCadastroPadrao.btnIncluirClick(Sender: TObject);
+begin
+  pnlPrincipal.ActiveCard := cardCadastro;
+end;
+
+end.
